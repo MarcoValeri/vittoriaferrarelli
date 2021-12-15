dd1f34202b274a20bb0eec37d0a9b12f1cab2328
Marco Valeri <marcovaleri@hotmail.it>
2021-12-15 08:06:57 +0000

    Create the footer

--- wp-content/themes/vittoriaferrarelli/footer.php
+++ wp-content/themes/vittoriaferrarelli/footer.php
@@ -1,14 +1,14 @@
-<footer>
+<footer class="footer">
     <!-- 
         Add a function that allows WordPress users add content in the footer by
         WordPress menu
     -->
     <?php wp_footer(); ?>
-    <!--
-    <p>
-        Made with &hearts; in London by 
-        <a href="https://devmarco.com" target="_blank">Marco Valeri</a> 
-        &copy; <?= date('Y'); ?> - All rights are reserved
-    </p>
-    -->
+    <section class="footer__content">
+        <p>
+            Made with <span class="heart">&hearts;</span> in London by 
+            <a class="link-no-decoration" href="https://devmarco.com" target="_blank">Marco Valeri</a> 
+            &copy; <?= date('Y'); ?> - All rights are reserved
+        </p>
+    </section>
 </footer>
