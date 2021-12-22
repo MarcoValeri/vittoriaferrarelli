a1463d38101547e6da35293f05df4085b3d60710
Marco Valeri <marcovaleri@hotmail.it>
2021-12-22 14:35:31 +0000

    Create template for single post

--- wp-content/themes/vittoriaferrarelli/single-post.php
+++ wp-content/themes/vittoriaferrarelli/single-post.php
@@ -3,7 +3,19 @@
 
 <!-- Add main section -->
 <main class="content">
-    <h1>Single post</h1>
+    <section class="content__container">
+        <section class="single-post__container">
+            <?php the_title('<h1 class="single-post__title">', '</h1>'); ?>
+            <section class="single-post__image">
+                <?php the_post_thumbnail(); ?>
+            </section>
+            <section class="single-post__content paragraph">
+                <?php the_content(); ?>
+            </section>
+        </section>
+    </section>
+    <aside class="aside">
+    </aside>
 </main>
 
 
